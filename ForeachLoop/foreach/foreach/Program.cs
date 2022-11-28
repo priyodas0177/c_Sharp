2f4587055918470299c93eba3189145f2259e933
priyidas0177 <priyodas0177@gmail.com>
2022-11-29 00:58:52 +0600

    for pc

---
+++ ForeachLoop/foreach/foreach/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+
+namespace @foreach
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string name;
+            string address;
+            int count=1;
+
+            byte[] array= new byte[2]; 
+             
+            foreach (byte a in array) {
+                Console.WriteLine("Person "+count);
+                Console.WriteLine("Enter your name ");
+                name = Console.ReadLine();
+                Console.WriteLine("Enter your Address ");
+                address = Console.ReadLine();
+                Console.WriteLine("My name is: "+name);
+                Console.WriteLine("My address is: "+address);
+                // Console.ReadLine();
+
+               
+                count ++;
+
+
+
+                // Console.WriteLine(a);
+            }
+           
+            Console.ReadLine();
+            
+
+
+        }
+    }
+}

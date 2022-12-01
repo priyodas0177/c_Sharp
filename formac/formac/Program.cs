a37aa325a7ec53dd6dc95836e3ea9abd71885c5a
priyidas0177 <priyodas0177@gmail.com>
2022-12-02 00:16:31 +0600

    for windows

---
+++ formac/formac/Program.cs
@@ -0,0 +1,45 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+
+namespace @foreach
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string name;
+            string address;
+            int count = 1;
+
+            byte[] array = new byte[3];
+
+            foreach (byte a in array)
+            {
+                Console.WriteLine("dELETE ");
+                Console.WriteLine("Person " + count);
+                Console.WriteLine("Enter your name ");
+                name = Console.ReadLine();
+                Console.WriteLine("Enter your Address ");
+                address = Console.ReadLine();
+                Console.WriteLine("My name is: " + name);
+                Console.WriteLine("My address is: " + address);
+                // Console.ReadLine();
+
+
+                count++;
+
+
+
+                // Console.WriteLine(a);
+            }
+
+            Console.ReadLine();
+
+
+
+        }
+    }
+}
+
+
